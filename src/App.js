720be47692e6e5aebaba72066556ff3706d23c38
timothy-tyk <timothytan94@gmail.com>
2022-07-14 17:14:31 +0800

    comfortable, card-images are rendered on screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
+import PlayingCard from "./PlayingCard";
 import { makeShuffledDeck } from "./utils.js";
+
+let imageArray = [];
 
 class App extends React.Component {
   constructor(props) {
@@ -11,9 +14,20 @@ class App extends React.Component {
       cardDeck: makeShuffledDeck(),
       // currCards holds the cards from the current round
       currCards: [],
+      //round winner
+      winner: "",
+      player1WinCount: 0,
+      player2WinCount: 0,
     };
   }
-
+  importAll = (r) => {
+    return r.keys().map(r);
+  };
+  componentWillMount() {
+    imageArray = this.importAll(
+      require.context("./cards/", false, /\.(png|jpe?g|svg)$/)
+    );
+  }
   dealCards = () => {
     this.setState((state) => ({
       // Remove last 2 cards from cardDeck
@@ -22,6 +36,41 @@ class App extends React.Component {
       currCards: state.cardDeck.slice(-2),
     }));
   };
+
+  compareCards = () => {
+    console.log(this.state.currCards);
+    if (this.state.currCards[0].rank > this.state.currCards[1].rank) {
+      this.setState({
+        winner: "Player 1",
+        player1WinCount: this.state.player1WinCount + 1,
+      });
+    } else if (this.state.currCards[1].rank > this.state.currCards[0].rank) {
+      this.setState({
+        winner: "Player 2",
+        player2WinCount: this.state.player2WinCount + 1,
+      });
+    } else {
+      this.setState({ winner: "Both" });
+    }
+  };
+
+  overallWinner = () => {
+    if (this.state.player1WinCount > this.state.player2WinCount) {
+      return "Player 1";
+    } else if (this.state.player2WinCount > this.state.player1WinCount) {
+      return "Player 2";
+    } else {
+      return "";
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    // Typical usage (don't forget to compare props):
+    if (this.state.currCards !== prevState.currCards) {
+      this.compareCards();
+      this.overallWinner();
+    }
+  }
 
   render() {
     const currCardElems = this.state.currCards.map(({ name, suit }) => (
@@ -35,9 +84,26 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h3>High Card 🚀</h3>
+          <PlayingCard currCard={this.state.currCards} />
           {currCardElems}
+          {/* <div>
+            {imageArray.map((image, index) => (
+              <img key={index} src={image} alt="info"></img>
+            ))}
+          </div> */}
+          {this.state.currCards.length > 0 ? (
+            <div>
+              {/* <img alt="" src={this.state.currCards[0].image} />
+              <img alt="" src={this.state.currCards[1].image} />
+              {console.log(this.state.currCards[0].image)} */}
+            </div>
+          ) : null}
           <br />
           <button onClick={this.dealCards}>Deal</button>
+          <h1>{this.state.winner}</h1>
+          <h3>Player 1 Score: {this.state.player1WinCount}</h3>
+          <h3>Player 2 Score: {this.state.player2WinCount}</h3>
+          <h2>Overall Winner: {this.overallWinner()}</h2>
         </header>
       </div>
     );
---
+++ src/PlayingCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+import { makeShuffledDeck } from "./utils.js";
+let imageArray = [];
+
+export default class PlayingCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cardImage1: "", cardImage2: "" };
+  }
+
+  importAll = (r) => {
+    return r.keys().map(r);
+  };
+  componentWillMount() {
+    imageArray = this.importAll(
+      require.context("./cards/", false, /\.(png|jpe?g|svg)$/)
+    );
+  }
+
+  getCorrectImage = () => {
+    let string1 = this.props.currCard[0].image;
+    console.log(string1);
+    for (let i = 0; i < imageArray.length; i++) {
+      if (imageArray[i].includes(string1)) {
+        this.setState({ cardImage1: imageArray[i] });
+      }
+    }
+    let string2 = this.props.currCard[1].image;
+    for (let i = 0; i < imageArray.length; i++) {
+      if (imageArray[i].includes(string2)) {
+        this.setState({ cardImage2: imageArray[i] });
+      }
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.currCard !== prevProps.currCard) {
+      this.getCorrectImage();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <img alt="" className="cards-png" src={this.state.cardImage1} />
+        <img alt="" className="cards-png" src={this.state.cardImage2} />
+      </div>
+    );
+  }
+}
